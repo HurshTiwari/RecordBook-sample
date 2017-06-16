ab40841f522c78d5142cec317207b0142653e75c
HurshTiwari <harshdude95@gmail.com>
2017-06-16 19:16:20 +0530

    Adding Account class.

---
+++ src/Account.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+/*
+ * Intended use : Account of an individual 
+ * Encapsulates the debit and credit process 
+ * Keeps track of total balance in the ledger
+ */
+public class Account {
+    private ArrayList<Integer> debit;
+	private ArrayList<Integer> credit;
+	int total;
+	Account(){
+		debit = new ArrayList<Integer>();
+		credit = new ArrayList<Integer>();
+		total=0;
+	}
+	
+	public void addDebit(int amount){
+		debit.add(amount);
+		total-=amount;
+	}
+	public void addCredit(int amount){
+		credit.add(amount);
+		total+=amount;
+	}
+	public void displayTotal(){
+		System.out.println(total);
+	}
+ 
+}
